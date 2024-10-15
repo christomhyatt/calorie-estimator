40487609ebb5f9e92c0f47c45ba44379c0c6f7c0
Chris Hyatt <chrishyatt@chriss-mbp.lan>
2024-10-15 15:59:53 -0600

    Deleting the bullshit

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python:3.9-slim
-WORKDIR /app
-COPY . /app
-RUN pip install --no-cache-dir -r requirements.txt
-RUN pip install gunicorn  # Add this line to install gunicorn
-EXPOSE 5000
-ENV FLASK_APP=CalorieEstimator.py
-CMD ["gunicorn", "--bind", "0.0.0.0:5000", "CalorieEstimator:app"]
--- historical_analysis.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Open csv file and pull in data
-# Add data to a workable dataframe
-# Filter out data to only look at activities = 'Running' + 'Trail Running'
-# Focus in on columns for date, activitiy type, distance, calories burned, time, pace, cadence
-# Calculate the past number of calories burned by
-# How to forecast pas 
